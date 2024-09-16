73f17ed6936fc9439ff6167bf317c54b0d3d73c7
bryanyee33 <bryanyee33@gmail.com>
2024-09-17 07:04:54 +0800

    Cache overlaps before resolving

--- collision.h
+++ collision.h
@@ -13,19 +13,19 @@
 // Is particle overlapping with a wall
 inline bool is_wall_overlap(Vec2 loc, int square_size, int radius);
 // Is particle colliding with a wall
-inline bool is_wall_collision(Vec2 loc, Vec2 vel, int square_size, int radius);
+inline bool is_wall_collision(std::vector<Particle> &particles, int idx, int square_size, int radius);
 // Make particle not collide with wall
 // PRECONDITION: Can be called with no preconditions
 inline void resolve_wall_collision(Vec2 loc, Vec2& vel, int square_size, int radius);
 // Are the particles overlapping
 inline bool is_particle_overlap(Vec2 loc1, Vec2 loc2, int radius);
 // Are the particles moving closer together
-inline bool is_particle_moving_closer(Vec2 loc1, Vec2 vel1, Vec2 loc2, Vec2 vel2);
+inline bool is_particle_moving_closer(std::vector<Particle> &particles, int idx1, int idx2);
 // Are the particles colliding
-inline bool is_particle_collision(Vec2 loc1, Vec2 vel1, Vec2 loc2, Vec2 vel2, int radius);
+inline bool is_particle_collision(std::vector<Particle> &particles, int idx1, int idx2, int radius);
 // Make particles not collide with each other
 // PRECONDITION: Must only be called if particles are overlapping
-inline void resolve_particle_collision(Vec2 loc1, Vec2& vel1, Vec2 loc2, Vec2& vel2);
+inline void resolve_particle_collision(std::vector<Particle> &particles, int idx1, int idx2);
 // Get the total energy of a group of particles
 inline double getEnergy(std::vector<Particle>& particles);
 // Get the total momentum of a group of particles
@@ -52,9 +52,10 @@ inline bool is_wall_overlap(Vec2 loc, int square_size, int radius) {
  * - square_size: The length of the simulation area
  * - radius: The radius of the particle
  */
-inline bool is_wall_collision(Vec2 loc, Vec2 vel, int square_size, int radius) {
-    return (loc.x - radius <= 0 && vel.x < 0) || (loc.x + radius >= square_size && vel.x > 0) ||
-            (loc.y - radius <= 0 && vel.y < 0) || (loc.y + radius >= square_size && vel.y > 0);
+inline bool is_wall_collision(std::vector<Particle> &particles, int idx, int square_size, int radius) {
+    Particle &p = particles[idx];
+    return (p.loc.x - radius <= 0 && p.vel.x < 0) || (p.loc.x + radius >= square_size && p.vel.x > 0) ||
+            (p.loc.y - radius <= 0 && p.vel.y < 0) || (p.loc.y + radius >= square_size && p.vel.y > 0);
 }
 
 /**
@@ -64,16 +65,17 @@ inline bool is_wall_collision(Vec2 loc, Vec2 vel, int square_size, int radius) {
  * - square_size: The length of the simulation area
  * - radius: The radius of the particle
  */
-inline void resolve_wall_collision(Vec2 loc, Vec2& vel, int square_size, int radius) {
-    if (loc.x - radius <= 0) {
-        vel.x = std::abs(vel.x);
-    } else if (loc.x + radius >= square_size) {
-        vel.x = -std::abs(vel.x);
+inline void resolve_wall_collision(std::vector<Particle> &particles, int idx, int square_size, int radius) {
+    Particle &p = particles[idx];
+    if (p.loc.x - radius <= 0) {
+        p.vel.x = std::abs(p.vel.x);
+    } else if (p.loc.x + radius >= square_size) {
+        p.vel.x = -std::abs(p.vel.x);
     }
-    if (loc.y - radius <= 0) {
-        vel.y = std::abs(vel.y);
-    } else if (loc.y + radius >= square_size) {
-        vel.y = -std::abs(vel.y);
+    if (p.loc.y - radius <= 0) {
+        p.vel.y = std::abs(p.vel.y);
+    } else if (p.loc.y + radius >= square_size) {
+        p.vel.y = -std::abs(p.vel.y);
     }
 }
 
@@ -96,8 +98,10 @@ inline bool is_particle_overlap(Vec2 loc1, Vec2 loc2, int radius) {
  * - loc2: The location of the second particle
  * - vel2: The velocity of the second particle
  */
-inline bool is_particle_moving_closer(Vec2 loc1, Vec2 vel1, Vec2 loc2, Vec2 vel2) {
-    return (vel2.x - vel1.x) * (loc2.x - loc1.x) + (vel2.y - vel1.y) * (loc2.y - loc1.y) < -0.0000001;
+inline bool is_particle_moving_closer(std::vector<Particle> &particles, int idx1, int idx2) {
+    Particle &p1 = particles[idx1], &p2 = particles[idx2];
+    return (p2.vel.x - p1.vel.x) * (p2.loc.x - p1.loc.x) +
+            (p2.vel.y - p1.vel.y) * (p2.loc.y - p1.loc.y) < -0.0000001;
 }
 
 /**
@@ -108,8 +112,8 @@ inline bool is_particle_moving_closer(Vec2 loc1, Vec2 vel1, Vec2 loc2, Vec2 vel2
  * - vel2: The velocity of the second particle
  * - radius: The radius of the particles
  */
-inline bool is_particle_collision(Vec2 loc1, Vec2 vel1, Vec2 loc2, Vec2 vel2, int radius) {
-    return is_particle_overlap(loc1, loc2, radius) && is_particle_moving_closer(loc1, vel1, loc2, vel2);
+inline bool is_particle_collision(std::vector<Particle> &particles, int idx1, int idx2, int radius) {
+    return is_particle_overlap(particles[idx1].loc, particles[idx2].loc, radius) && is_particle_moving_closer(particles, idx1, idx2);
 }
 
 /**
@@ -119,18 +123,20 @@ inline bool is_particle_collision(Vec2 loc1, Vec2 vel1, Vec2 loc2, Vec2 vel2, in
  * - loc2: The location of the second particle
  * - vel2: The velocity of the second particle
  */
-inline void resolve_particle_collision(Vec2 loc1, Vec2& vel1, Vec2 loc2, Vec2& vel2) {
-    double dx = loc2.x - loc1.x;
-    double dy = loc2.y - loc1.y;
+inline void resolve_particle_collision(std::vector<Particle> &particles, int idx1, int idx2) {
+    Particle &p1 = particles[idx1], &p2 = particles[idx2];
+    double dx = p2.loc.x - p1.loc.x;
+    double dy = p2.loc.y - p1.loc.y;
+
 
     // Calculate the new velocities of the particles after the collision
-    double collision_scale = std::min(0.0, (vel2.x - vel1.x) * dx + (vel2.y - vel1.y) * dy) /
+    double collision_scale = std::min(0.0, (p2.vel.x - p1.vel.x) * dx + (p2.vel.y - p1.vel.y) * dy) /
             (dx * dx + dy * dy);
 
-    vel1.x += collision_scale * dx;
-    vel1.y += collision_scale * dy;
-    vel2.x -= collision_scale * dx;
-    vel2.y -= collision_scale * dy;
+    p1.vel.x += collision_scale * dx;
+    p1.vel.y += collision_scale * dy;
+    p2.vel.x -= collision_scale * dx;
+    p2.vel.y -= collision_scale * dy;
 }
 
 /**
--- sim.cc
+++ sim.cc
@@ -3,6 +3,8 @@
 #include <cmath>
 #include <fstream>
 #include <vector>
+#include <unordered_set>
+#include <stack>
 
 #include "collision.h"
 #include "io.h"
@@ -15,36 +17,43 @@ void print_particle(Particle &p) {
 inline void update_grid(int grid_width, int grid_count,
         std::vector<std::vector<std::vector<int>>> &grid, std::vector<Particle> &particles) {
     //TODO
-    #pragma omp parallel for shared(grid, grid_count) if(grid_count >= 10000)
+    #pragma omp parallel for shared(grid, grid_count) collapse(2) if(grid_count >= 100000)
     for (int i = 0; i < grid_count; i++) {
         for (int j = 0; j < grid_count; j++) {
             grid[i][j].clear();
         }
     }
 
-    int count = (int) particles.size();
-    //TODO
-    #pragma omp parallel for shared(grid, grid_count, count) if(count >= 10000)
-    for (int i = 0; i < count; i++) {
+    // Parallelising is slower since particles.size() is too small,
+    // and since updating grid needs critical section
+    for (int i = 0; i < (int) particles.size(); i++) {
         int row = std::max(0, int(particles[i].loc.y / grid_width));
         row = std::min(row, grid_count - 1);
         int col = std::max(0, int(particles[i].loc.x / grid_width));
         col = std::min(col, grid_count - 1);
 
-        #pragma omp critical
         grid[row][col].push_back(i);
     }
 }
 
-inline bool check_and_resolve_particles(std::vector<int> &vec_to_check, std::vector<Particle> &particles, int particle_idx, int radius) {
+inline void add_overlaps(std::vector<int> &vec_to_check, std::vector<Particle> &particles, int particle_idx, std::vector<std::vector<int>> &overlaps, int radius) {
+    for (int i : vec_to_check) {
+        if (i != particle_idx && is_particle_overlap(particles[particle_idx].loc, particles[i].loc, radius)) {
+            #pragma omp critical
+            overlaps[particle_idx].push_back(i);
+        }
+    }
+}
+
+inline bool check_and_resolve_particles(std::vector<int> &vec_to_check, std::vector<Particle> &particles, int particle_idx, int square_size, int radius) {
     bool unresolved = false;
 
     for (int i : vec_to_check) {
-        Particle &p = particles[particle_idx];
-        Particle &o = particles[i];
-        if (i != particle_idx && is_particle_collision(p.loc, p.vel, o.loc, o.vel, radius)) {
-            #pragma omp critical
-            resolve_particle_collision(p.loc, p.vel, o.loc, o.vel);
+        if (i == -1 && is_wall_collision(particles, particle_idx, square_size, radius)) {
+            resolve_wall_collision(particles, particle_idx, square_size, radius);
+            unresolved = true;
+        } else if (i != -1 && is_particle_moving_closer(particles, particle_idx, i)) {
+            resolve_particle_collision(particles, particle_idx, i);
             unresolved = true;
         }
     }
@@ -91,48 +100,108 @@ int main(int argc, char* argv[]) {
         }
         update_grid(grid_width, grid_count, grid, particles);
 
-        // Velocity update
-        bool unresolved = true;
-        while (unresolved) {
-            unresolved = false;
-            
-            #pragma omp parallel for shared(grid, particles) collapse(2) reduction(|| : unresolved)
-            for (int row = 0; row < grid_count; row++) {
-                for (int col = 0; col < grid_count; col++) {
-                    for (int i : grid[row][col]) {
-                        // Find which quadrant of grid box the particle is in, and store the resulting indexes to check
-                        // No need critical section since location is fixed
-                        int y_check_idx = row + ((std::fmod(particles[i].loc.y, grid_width) >= grid_width / 2.0) << 1) - 1; // -1 for down, +1 for up, added to row
-                        int x_check_idx = col + ((std::fmod(particles[i].loc.x, grid_width) >= grid_width / 2.0) << 1) - 1; // -1 for left, +1 for right, added to col
-                        bool y_check_idx_valid = 0 <= y_check_idx && y_check_idx < grid_count;
-                        bool x_check_idx_valid = 0 <= x_check_idx && x_check_idx < grid_count;
+        // Find overlaps
+        std::vector<std::vector<int>> overlaps(params.param_particles);
 
-                        // Check for collisions
-                        // Same grid box
-                        unresolved = unresolved || check_and_resolve_particles(grid[row][col], particles, i, params.param_radius);
-                        
-                        if (x_check_idx_valid) {
-                            unresolved = unresolved || check_and_resolve_particles(grid[row][x_check_idx], particles, i, params.param_radius);
-                        }
-                        if (y_check_idx_valid) {
-                            unresolved = unresolved || check_and_resolve_particles(grid[y_check_idx][col], particles, i, params.param_radius);
-                        }
-                        
-                        // Diagonally opposite grid box
-                        if (y_check_idx_valid && x_check_idx_valid) {
-                            unresolved = unresolved || check_and_resolve_particles(grid[y_check_idx][x_check_idx], particles, i, params.param_radius);
-                        }
-                        // If last (or 2nd last) row / col; or Out of bounds for at least 1 axis
-                        if ((row >= possible_wall_collision_max || col >= possible_wall_collision_max || !y_check_idx_valid || !x_check_idx_valid) &&
-                                is_wall_collision(particles[i].loc, particles[i].vel, params.square_size, params.param_radius)) {
-                            #pragma omp critical
-                            resolve_wall_collision(particles[i].loc, particles[i].vel, params.square_size, params.param_radius);
-                            unresolved = true;
-                        }
+        #pragma omp parallel for shared(grid, overlaps, particles) collapse(2)
+        for (int row = 0; row < grid_count; row++) {
+            for (int col = 0; col < grid_count; col++) {
+                for (int i : grid[row][col]) {
+                    // Find which quadrant of grid box the particle is in, and store the resulting indexes to check
+                    // No need critical section since location is fixed
+                    int y_check_idx = row + ((std::fmod(particles[i].loc.y, grid_width) >= grid_width / 2.0) << 1) - 1; // -1 for down, +1 for up, added to row
+                    int x_check_idx = col + ((std::fmod(particles[i].loc.x, grid_width) >= grid_width / 2.0) << 1) - 1; // -1 for left, +1 for right, added to col
+                    bool y_check_idx_valid = 0 <= y_check_idx && y_check_idx < grid_count;
+                    bool x_check_idx_valid = 0 <= x_check_idx && x_check_idx < grid_count;
+
+                    // Find overlaps
+                    // Same grid box
+                    add_overlaps(grid[row][col], particles, i, overlaps, params.param_radius);
+                    
+                    if (x_check_idx_valid) {
+                        add_overlaps(grid[row][x_check_idx], particles, i, overlaps, params.param_radius);
+                    }
+                    if (y_check_idx_valid) {
+                        add_overlaps(grid[y_check_idx][col], particles, i, overlaps, params.param_radius);
+                    }
+                    
+                    // Diagonally opposite grid box
+                    if (y_check_idx_valid && x_check_idx_valid) {
+                        add_overlaps(grid[y_check_idx][x_check_idx], particles, i, overlaps, params.param_radius);
+                    }
+                    // If last (or 2nd last) row / col; or Out of bounds for at least 1 axis
+                    if ((row >= possible_wall_collision_max || col >= possible_wall_collision_max || !y_check_idx_valid || !x_check_idx_valid) &&
+                            is_wall_overlap(particles[i].loc, params.square_size, params.param_radius)) {
+                        #pragma omp critical
+                        overlaps[i].push_back(-1);
                     }
                 }
             }
         }
+
+        // Velocity update
+
+        // ARBITRARY ORDER [Fastest for Small]
+        // bool unresolved = true;
+        // while (unresolved) {
+        //     unresolved = false;
+        //     for (int i = 0; i < params.param_particles; i++) {
+        //         unresolved = check_and_resolve_particles(overlaps[i], particles, i, params.square_size, params.param_radius) || unresolved;
+        //     }
+        // }
+
+        // ARBITRARY REPEAT [Fastest for Medium & Large]
+        bool unresolved = true;
+        while (unresolved) {
+            unresolved = false;
+            for (int i = 0; i < params.param_particles; i++) {
+                if (check_and_resolve_particles(overlaps[i], particles, i, params.square_size, params.param_radius)) {
+                    while (check_and_resolve_particles(overlaps[i], particles, i, params.square_size, params.param_radius));
+                    unresolved = true;
+                }
+            }
+        }
+
+        // RESOLVE BY CLUSTER [Slowest, but took the longest time to make :( - too much overhead]
+        // std::unordered_set<int> visited; // contains elements of all resolved clusters
+        // std::unordered_set<int> cluster_visited; // contains elements only within current cluster
+        // std::stack<int> stack; // DFS stack
+
+        // for (int i = 0; i < params.param_particles; i++) {
+        //     if (visited.find(i) == visited.end()) {
+        //         // cluster not yet visited
+        //         stack.push(i);
+
+        //         // inside cluster
+        //         while (!stack.empty()) {
+        //             int curr = stack.top();
+        //             stack.pop();
+
+        //             if (cluster_visited.find(curr) == cluster_visited.end()) {
+        //                 // cluster element not yet visited
+        //                 if (check_and_resolve_particles(overlaps[curr], particles, curr, params.square_size, params.param_radius)) {
+        //                     // have unresolved; redo DFS starting from this element
+        //                     cluster_visited = std::unordered_set<int>();
+        //                     stack = std::stack<int>();
+                            
+        //                     // keep resolving until no more
+        //                     while (check_and_resolve_particles(overlaps[curr], particles, curr, params.square_size, params.param_radius));
+        //                 }
+
+        //                 // DFS
+        //                 cluster_visited.insert(curr);
+        //                 for (int neighbour : overlaps[curr]) {
+        //                     if (neighbour != -1) {
+        //                         stack.push(neighbour);
+        //                     }
+        //                 }
+        //             }
+        //         }
+
+        //         visited.merge(cluster_visited); // combine with overall visited set
+        //         cluster_visited = std::unordered_set<int>();
+        //     }
+        // }
 
         #if CHECK == 1
             // Check final positions
